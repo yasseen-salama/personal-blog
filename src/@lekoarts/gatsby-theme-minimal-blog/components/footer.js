c8c0313f7972cd1b2b16cc8bebb52571c099c251
Yasseen Salama <y.salama@tdsoftware.de>
2022-11-23 22:00:29 +0100

    shadow footer

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.js
@@ -0,0 +1,29 @@
+/** @jsx jsx */
+import { jsx, Link } from "theme-ui"
+
+const Footer = () => {
+  const siteTitle  =  "Yasseen Salamaaaa";
+
+  return (
+    <footer
+      sx={{
+        boxSizing: `border-box`,
+        display: `flex`,
+        justifyContent: `space-between`,
+        mt: [6],
+        color: `secondary`,
+        a: {
+          variant: `links.secondary`,
+        },
+        flexDirection: [`column`, `column`, `row`],
+        variant: `dividers.top`,
+      }}
+    >
+      <div>
+        &copy; {new Date().getFullYear()} by {siteTitle}. All rights reserved.
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
