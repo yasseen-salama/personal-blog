fb8acf8c3a66cee03d7fcf6b1ba138deea54e0b6
Yasseen Salama <y.salama@tdsoftware.de>
2022-11-23 01:11:06 +0100

    andorid logos

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,14 +66,14 @@ module.exports = {
         display: `standalone`,
         icons: [
           {
-            // src: `/android-chrome-192x192.png`,
-            // sizes: `192x192`,
-            // type: `image/png`,
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
           },
           {
-            // src: `/android-chrome-512x512.png`,
-            // sizes: `512x512`,
-            // type: `image/png`,
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
           },
         ],
       },
